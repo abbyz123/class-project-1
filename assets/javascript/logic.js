4e635b722da6f09492fc97d31b95af1f41f22d79
mengyuan zhang <zhangm8976@student.laccd.edu>
2019-06-29 16:11:11 -0700

    adjust bubble bouncing rule so that it bounces back exactly when it hits the canvas boundary; add comments to the code for readability

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,22 +1,27 @@
-let bubbleDiameter = 150;
-let bubbleArr = [];
-let bubbleNum = 6;
-let speedOpt = [1, 2];
-let speedDir = [-1, -1];
+// global variables
+let bubbleDiameter = 150;                           // bubble diameter
+let bubbleArr = [];                                 // bubble arrays
+let bubbleNum = 6;                                  // number of bubbles
+let speedOpt = [1, 2];                              // bubble speed options
+let speedDir = [-1, 1];                             // bubble speed directions
 
+// setup p5 canvas
 function setup() {
-    let myCanvas = createCanvas(1024, 768);
-    myCanvas.parent('bubbleCanvas');
-    frameRate(25);
+    // Setup canvas
+    let myCanvas = createCanvas(1024, 768);         // 1024 x 768 for standard laptop
+    myCanvas.parent('bubbleCanvas');                // canvas under the container
+    frameRate(25);                                  // frame rate = 25 fps
     
+    // create bubbles with random position and push the bubble objects into global array
     for (i = 0; i < bubbleNum; i++) {
         bubble = new Bubbles(i, random(bubbleDiameter, width-bubbleDiameter), random(bubbleDiameter, height-bubbleDiameter), bubbleDiameter, random(speedDir) * random(speedOpt), random(speedDir) * random(speedOpt), bubbleArr, "color");
         bubbleArr.push(bubble);
     }
 }
 
-
+// Bubble class 
 class Bubbles {
+    // Bubble class constructor
     constructor(id, centerX, centerY, diameter, speedX, speedY, bubbleArr, color="white") {
         this.centerX = centerX;
         this.centerY = centerY;
@@ -28,29 +33,31 @@ class Bubbles {
         this.id = id;
     }
 
+    // move the bubble with pre-defined speed
     move() {
+        // update the bubble position with speed
         this.centerX += this.speedX;
         this.centerY += this.speedY;
 
-        if ((this.centerX - this.diameter) < 0 || (this.centerX + this.diameter) > width) {
+        // if the bubble is hitting the canvas boundary, change the speed direction
+        if ((this.centerX - this.diameter/2) < 0 || (this.centerX + this.diameter/2) > width) {
             this.speedX *= -1;
         }
-        if ((this.centerY - this.diameter) < 0 || (this.centerY + this.diameter) > height) {
+        if ((this.centerY - this.diameter/2) < 0 || (this.centerY + this.diameter/2) > height) {
             this.speedY *= -1;
         }
     }
 
-    // move method
+    // show bubble
     show() {
-        // print("entering function show");
-        stroke(255);
-        strokeWeight(5);
-        noFill();
+        stroke(255);                // bubble stroke color white (can be adjusted)
+        strokeWeight(5);            // bubble stroke weight 5 px
+        noFill();                   // no fill 
         ellipse(this.centerX, this.centerY, this.diameter, this.diameter);
     }
 
+    // bouncing algorithm for bubbles
     collide() {
-        //print("entering function collide");
         // check if other bubbles are colliding with this bubble
         for (let idx = this.id + 1; idx < this.otherBubbles.length; idx++) {
             let dx = this.centerX - this.otherBubbles[idx].centerX;
@@ -58,32 +65,43 @@ class Bubbles {
             let dist = sqrt(dx * dx + dy * dy);
             let minDist = this.diameter/2 + this.otherBubbles[idx].diameter/2;
             if (dist > minDist) {
+                // do nothing if this bubble is not colliding with other bubbles
                 continue;
             } else {
+                // update the speed for both bubbles according to the colliding directions
+                // This is a pseudo-momentum conservative model. It only works when 
+                // the bubble moves very very slow
                 let newSpeedX = dx / max(abs(dx), abs(dy));
                 let newSpeedY = dy / max(abs(dx), abs(dy));
                 this.speedX = newSpeedX;
                 this.speedY = newSpeedY;
                 this.otherBubbles[idx].speedX = -newSpeedX;
                 this.otherBubbles[idx].speedY = -newSpeedY;
-                print("collide");
-                print([this.speedX, this.speedY]);
             }
         }
     }
 
+    // stop this bubbles
     stop() {
         this.speedX = 0;
         this.speedY = 0;
     }
+}
 
-    hide() {
-        noStroke();
-        noFill();
+// Define 2D euclidean distance between two coordinates
+function dist(x1, y1, x2, y2) {
+    return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+}
+
+// stop bubble if the mouse is hovering on it
+function stopBubble() {
+    for(let i = 0; i < bubbleArr.length; i++) {
+        let mouseDist = dist(mouseX, mouseY, bubbleArr[i].centerX, bubbleArr[i].centerY);
     }
 }
 
 function draw() {
+    // background color black (can be adjusted)
     background('#222222');
     for (let i = 0; i < bubbleArr.length; i++) {
         bubbleArr[i].show();
