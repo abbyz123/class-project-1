4642d29d2e0346e2e6174579c4e518a01b1f4236
mengyuan zhang <zhangm8976@student.laccd.edu>
2019-06-29 16:45:59 -0700

    bubble stops when mouse is over it

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -11,10 +11,10 @@ function setup() {
     let myCanvas = createCanvas(1024, 768);         // 1024 x 768 for standard laptop
     myCanvas.parent('bubbleCanvas');                // canvas under the container
     frameRate(25);                                  // frame rate = 25 fps
-    
+
     // create bubbles with random position and push the bubble objects into global array
     for (i = 0; i < bubbleNum; i++) {
-        bubble = new Bubbles(i, random(bubbleDiameter, width-bubbleDiameter), random(bubbleDiameter, height-bubbleDiameter), bubbleDiameter, random(speedDir) * random(speedOpt), random(speedDir) * random(speedOpt), bubbleArr, "color");
+        bubble = new Bubbles(i, random(bubbleDiameter, width - bubbleDiameter), random(bubbleDiameter, height - bubbleDiameter), bubbleDiameter, random(speedDir) * random(speedOpt), random(speedDir) * random(speedOpt), bubbleArr, "color");
         bubbleArr.push(bubble);
     }
 }
@@ -22,7 +22,7 @@ function setup() {
 // Bubble class 
 class Bubbles {
     // Bubble class constructor
-    constructor(id, centerX, centerY, diameter, speedX, speedY, bubbleArr, color="white") {
+    constructor(id, centerX, centerY, diameter, speedX, speedY, bubbleArr, color = "white") {
         this.centerX = centerX;
         this.centerY = centerY;
         this.diameter = diameter;
@@ -31,19 +31,26 @@ class Bubbles {
         this.color = color;
         this.otherBubbles = bubbleArr;
         this.id = id;
+        this.stopOrNot = false;
     }
 
     // move the bubble with pre-defined speed
     move() {
+        // if the bubble is stopped, give it random kick!
+        if(true === this.stopOrNot) {
+            this.stopOrNot = false;
+            this.speedX = random(speedOpt) * random(speedDir);
+            this.speedY = random(speedOpt) * random(speedDir);
+        }
         // update the bubble position with speed
         this.centerX += this.speedX;
         this.centerY += this.speedY;
 
         // if the bubble is hitting the canvas boundary, change the speed direction
-        if ((this.centerX - this.diameter/2) < 0 || (this.centerX + this.diameter/2) > width) {
+        if ((this.centerX - this.diameter / 2) < 0 || (this.centerX + this.diameter / 2) > width) {
             this.speedX *= -1;
         }
-        if ((this.centerY - this.diameter/2) < 0 || (this.centerY + this.diameter/2) > height) {
+        if ((this.centerY - this.diameter / 2) < 0 || (this.centerY + this.diameter / 2) > height) {
             this.speedY *= -1;
         }
     }
@@ -63,7 +70,7 @@ class Bubbles {
             let dx = this.centerX - this.otherBubbles[idx].centerX;
             let dy = this.centerY - this.otherBubbles[idx].centerY;
             let dist = sqrt(dx * dx + dy * dy);
-            let minDist = this.diameter/2 + this.otherBubbles[idx].diameter/2;
+            let minDist = this.diameter / 2 + this.otherBubbles[idx].diameter / 2;
             if (dist > minDist) {
                 // do nothing if this bubble is not colliding with other bubbles
                 continue;
@@ -85,19 +92,13 @@ class Bubbles {
     stop() {
         this.speedX = 0;
         this.speedY = 0;
+        this.stopOrNot = true;
     }
 }
 
 // Define 2D euclidean distance between two coordinates
-function dist(x1, y1, x2, y2) {
+function distCalc(x1, y1, x2, y2) {
     return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-}
-
-// stop bubble if the mouse is hovering on it
-function stopBubble() {
-    for(let i = 0; i < bubbleArr.length; i++) {
-        let mouseDist = dist(mouseX, mouseY, bubbleArr[i].centerX, bubbleArr[i].centerY);
-    }
 }
 
 function draw() {
@@ -106,6 +107,11 @@ function draw() {
     for (let i = 0; i < bubbleArr.length; i++) {
         bubbleArr[i].show();
         bubbleArr[i].collide();
-        bubbleArr[i].move();
+        let mouseDist = distCalc(mouseX, mouseY, bubbleArr[i].centerX, bubbleArr[i].centerY);
+        if(mouseDist < bubbleArr[i].diameter / 2){
+           bubbleArr[i].stop();
+        } else {
+            bubbleArr[i].move();
+        }
     }
 }
