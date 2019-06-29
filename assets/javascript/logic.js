34945137bd5cbf97db6f3ef344e15ad93137b124
mengyuan zhang <zhangm8976@student.laccd.edu>
2019-06-29 14:49:59 -0700

    add features of bubbles colliding

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,47 +1,74 @@
-let bubbleRadius = 150;
+let bubbleDiameter = 150;
 let bubbleArr = [];
-let bubbleNum = 4;
-let speedOpt = [1, 1];
+let bubbleNum = 6;
+let speedOpt = [1, 2];
 let speedDir = [-1, -1];
 
 function setup() {
     let myCanvas = createCanvas(1024, 768);
     myCanvas.parent('bubbleCanvas');
-    frameRate(20);
+    frameRate(25);
     
     for (i = 0; i < bubbleNum; i++) {
-        bubble = new Bubbles(random(bubbleRadius, width-bubbleRadius), random(bubbleRadius, height-bubbleRadius), bubbleRadius, random(speedDir) * random(speedOpt), random(speedDir) * random(speedOpt), "color");
+        bubble = new Bubbles(i, random(bubbleDiameter, width-bubbleDiameter), random(bubbleDiameter, height-bubbleDiameter), bubbleDiameter, random(speedDir) * random(speedOpt), random(speedDir) * random(speedOpt), bubbleArr, "color");
         bubbleArr.push(bubble);
     }
 }
 
 
 class Bubbles {
-    constructor(centerX, centerY, radius, speedX, speedY, color) {
+    constructor(id, centerX, centerY, diameter, speedX, speedY, bubbleArr, color="white") {
         this.centerX = centerX;
         this.centerY = centerY;
-        this.radius = radius;
+        this.diameter = diameter;
         this.speedX = speedX;
         this.speedY = speedY;
         this.color = color;
-    }
-    // move method
-    show() {
-        stroke(255);
-        strokeWeight(5);
-        noFill();
-        ellipse(this.centerX, this.centerY, this.radius, this.radius);
+        this.otherBubbles = bubbleArr;
+        this.id = id;
     }
 
     move() {
         this.centerX += this.speedX;
         this.centerY += this.speedY;
 
-        if ((this.centerX - this.radius) < 0 || (this.centerX + this.radius) > width) {
+        if ((this.centerX - this.diameter) < 0 || (this.centerX + this.diameter) > width) {
             this.speedX *= -1;
         }
-        if ((this.centerY - this.radius) < 0 || (this.centerY + this.radius) > height) {
+        if ((this.centerY - this.diameter) < 0 || (this.centerY + this.diameter) > height) {
             this.speedY *= -1;
+        }
+    }
+
+    // move method
+    show() {
+        // print("entering function show");
+        stroke(255);
+        strokeWeight(5);
+        noFill();
+        ellipse(this.centerX, this.centerY, this.diameter, this.diameter);
+    }
+
+    collide() {
+        //print("entering function collide");
+        // check if other bubbles are colliding with this bubble
+        for (let idx = this.id + 1; idx < this.otherBubbles.length; idx++) {
+            let dx = this.centerX - this.otherBubbles[idx].centerX;
+            let dy = this.centerY - this.otherBubbles[idx].centerY;
+            let dist = sqrt(dx * dx + dy * dy);
+            let minDist = this.diameter/2 + this.otherBubbles[idx].diameter/2;
+            if (dist > minDist) {
+                continue;
+            } else {
+                let newSpeedX = dx / max(abs(dx), abs(dy));
+                let newSpeedY = dy / max(abs(dx), abs(dy));
+                this.speedX = newSpeedX;
+                this.speedY = newSpeedY;
+                this.otherBubbles[idx].speedX = -newSpeedX;
+                this.otherBubbles[idx].speedY = -newSpeedY;
+                print("collide");
+                print([this.speedX, this.speedY]);
+            }
         }
     }
 
@@ -58,8 +85,9 @@ class Bubbles {
 
 function draw() {
     background('#222222');
-    for (i = 0; i < bubbleArr.length; i++) {
+    for (let i = 0; i < bubbleArr.length; i++) {
         bubbleArr[i].show();
+        bubbleArr[i].collide();
         bubbleArr[i].move();
     }
 }
